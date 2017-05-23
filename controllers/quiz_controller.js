5ad40296e10e6fda8f79dde6fb06cda37726ef03
anapereirab <ana.pereirab@alumnos.upm.es>
2017-05-23 13:48:02 +0200

    Sesion8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -305,7 +305,7 @@ exports.checkRandom = function (req, res, next) {
             answer: answer,
             score: acertadas
         });
-
+	acertadas=0;
     } else {
         next(new Error('No existe ningún quiz con id=' + quizId));
     }
