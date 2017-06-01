33a166a2f1e3a9d059b930e470e51cd458bc48cf
anapereirab <ana.pereirab@alumnos.upm.es>
2017-06-01 16:53:22 +0200

    Practica 52 local

--- models/index.js
+++ models/index.js
@@ -13,8 +13,8 @@ var url, storage;
 
 if (!process.env.DATABASE_URL) {
 	console.log("No hay process.env");
-    //url = "sqlite:///";
-    url = "postgres://rlgdvzqrbqvrno:a77a3bcc10f25aa67d424cf223d633b5c02176443b4637a9abb87d5d0955ecf0@ec2-54-225-118-55.compute-1.amazonaws.com:5432/dfm4k7kj9635mn"
+    url = "sqlite:///";
+    //url = "postgres://rlgdvzqrbqvrno:a77a3bcc10f25aa67d424cf223d633b5c02176443b4637a9abb87d5d0955ecf0@ec2-54-225-118-55.compute-1.amazonaws.com:5432/dfm4k7kj9635mn"
     storage = "quiz.sqlite";
     console.log("url: "+url);
 } else {
